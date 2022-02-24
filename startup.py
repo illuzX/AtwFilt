7b68e18bedbed6f8402b6bdc53509eef94a1def1
illuzX <98640218+illuzX@users.noreply.github.com>
2022-02-24 20:22:41 +0530

    Update startup.py

--- startup.py
+++ startup.py
@@ -31,13 +31,7 @@ class AtwFilt(object):
  » /logger - Get Bot Logs
  » /channel - Add Channel List"""
  
-   ADD_YOUR_GROUP= """**__{mention}, Check Movie Name In #Google and Try ! Then No Results Add Movie Year and Try , Again No Results ? It's Not Available In Our Database Or Movie Not Released !
-   
-{mention}, മൂവിയുടെ പേര് മാത്രം #Google നോക്കിയിട്ട് ഒന്ന് കൂടെ അടിച്ചു നോക്ക്..
-
-എന്നിട്ടും കിട്ടിയില്ലെങ്കിൽ പേരിന്റെ കൂടെ മൂവി ഇറങ്ങിയ വർഷം കൂടി അടിച്ചു നോക്ക് 😁
-
-എനിയും കിട്ടിയില്ലെങ്കിൽ ആ മൂവി ഞങ്ങളുടെ ഡാറ്റാബേസിൽ ഇല്ല, അല്ലെങ്കിൽ ആ മൂവി ഇറങ്ങിയിട്ടില്ല എന്ന് മനസ്സിലാക്കു__ **"""
+   ADD_YOUR_GROUP= """**__{mention}, Check Movie Name In #Google and Try ! Then No Results Add Movie Year and Try , Again No Results ? It's Not Available In Our Database Or Movie Not Released✅__ **"""
  
     SPELL_CHECK = """
 Hello 👋〘 {mention} 〙,
