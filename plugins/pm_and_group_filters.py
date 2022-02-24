b74baed7aab639b4f1effb4b137ea8f95a7cf23e
illuzX <98640218+illuzX@users.noreply.github.com>
2022-02-24 11:53:34 +0530

    update pm_and_group_filters.py

--- plugins/pm_and_group_filters.py
+++ plugins/pm_and_group_filters.py
@@ -1,16 +1,16 @@
-from pyrogram import Client as LuciferMoringstar_Robot, filters as Worker
-from LuciferMoringstar_Robot.modules.autofilter import group_filters, pm_autofilter
+from pyrogram import Client as illuzX, filters as Worker
+from plugins.modules.autofilter import group_filters, pm_autofilter
 from config import AUTH_GROUPS, AUTH_USERS
-from LuciferMoringstar_Robot.database.broadcast_db import Database
+from plugins.database.broadcast_db import Database
 
 db = Database()
 
-@LuciferMoringstar_Robot.on_message(Worker.text & Worker.group & Worker.incoming & Worker.chat(AUTH_GROUPS) if AUTH_GROUPS else Worker.text & Worker.group & Worker.incoming)
+@illuzX.on_message(Worker.text & Worker.group & Worker.incoming & Worker.chat(AUTH_GROUPS) if AUTH_GROUPS else Worker.text & Worker.group & Worker.incoming)
 async def groupfilters(client, message):
     await group_filters(client, message)
 
 
-@LuciferMoringstar_Robot.on_message(Worker.text & Worker.private & Worker.incoming & Worker.user(AUTH_USERS) if AUTH_USERS else Worker.text & Worker.private & Worker.incoming)
+@illuzX.on_message(Worker.text & Worker.private & Worker.incoming & Worker.user(AUTH_USERS) if AUTH_USERS else Worker.text & Worker.private & Worker.incoming)
 async def pm_filters(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
