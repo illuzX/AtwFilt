27f2da7cc0137b70ba33d3212789f2f42c780985
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-01 22:45:29 +0530

    update inline.py

--- plugins/modules/inline.py
+++ plugins/modules/inline.py
@@ -86,8 +86,8 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('Support Group', url='t.me/mvbzzeroffical'),
+        InlineKeyboardButton('More Botz', url='t.me/mvbbots')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
