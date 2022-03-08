5f157add36449cb0190d1f8f91945f86fe187c01
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-08 21:54:29 +0530

    update inline.py

--- plugins/modules/inline.py
+++ plugins/modules/inline.py
@@ -1,20 +1,39 @@
 import logging
-from pyrogram import Client as illuzX, filters as Worker, emoji
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from plugins.database._utils import get_size
-from plugins.database.autofilter_db import get_search_results, is_subscribed
-from config import CACHE_TIME, AUTH_USERS, FORCES_SUB, CUSTOM_FILE_CAPTION
+from pyrogram import Client, emoji, filters
+from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
+from database.ia_filterdb import get_search_results
+from utils import is_subscribed, get_size, temp
+from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+
 logger = logging.getLogger(__name__)
-cache_time = 0 if AUTH_USERS or FORCES_SUB else CACHE_TIME
+cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
 
+async def inline_users(query: InlineQuery):
+    if AUTH_USERS:
+        if query.from_user and query.from_user.id in AUTH_USERS:
+            return True
+        else:
+            return False
+    if query.from_user and query.from_user.id not in temp.BANNED_USERS:
+        return True
+    return False
 
-@illuzX.on_inline_query(Worker.user(AUTH_USERS) if AUTH_USERS else None)
+@Client.on_inline_query()
 async def answer(bot, query):
-
-    if FORCES_SUB and not await is_subscribed(bot, query):
+    """Show search results for given inline query"""
+    
+    if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You Have To Subscribe My Channel To Use The Bot',
+                           switch_pm_text='okDa',
+                           switch_pm_parameter="hehe")
+        return
+
+    if AUTH_CHANNEL and not await is_subscribed(bot, query):
+        await query.answer(results=[],
+                           cache_time=0,
+                           switch_pm_text='You have to subscribe my channel to use the bot',
                            switch_pm_parameter="subscribe")
         return
 
@@ -29,22 +48,23 @@ async def answer(bot, query):
 
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
-    files, next_offset = await get_search_results(string,
+    files, next_offset, total = await get_search_results(string,
                                                   file_type=file_type,
                                                   max_results=10,
                                                   offset=offset)
+
     for file in files:
         title=file.file_name
         size=get_size(file.file_size)
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
             except Exception as e:
-                print(e)
+                logger.exception(e)
                 f_caption=f_caption
         if f_caption is None:
-            f_caption = f"📁 **Title:** `{file.file_name}`"
+            f_caption = f"{file.file_name}"
         results.append(
             InlineQueryResultCachedDocument(
                 title=file.file_name,
@@ -54,10 +74,9 @@ async def answer(bot, query):
                 reply_markup=reply_markup))
 
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results"
+        switch_pm_text = f"{emoji.FILE_FOLDER} Results - {total}"
         if string:
             switch_pm_text += f" for {string}"
-
         try:
             await query.answer(results=results,
                            is_personal = True,
@@ -65,14 +84,11 @@ async def answer(bot, query):
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="start",
                            next_offset=str(next_offset))
+        except QueryIdInvalid:
+            pass
         except Exception as e:
             logging.exception(str(e))
-            await query.answer(results=[], is_personal=True,
-                           cache_time=cache_time,
-                           switch_pm_text=str(e)[:63],
-                           switch_pm_parameter="error")
     else:
-
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
             switch_pm_text += f' for "{string}"'
@@ -85,12 +101,13 @@ async def answer(bot, query):
 
 
 def get_reply_markup(query):
-    buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/mvbzzeroffical'),
-        InlineKeyboardButton('More Botz', url='t.me/mvbbots')
-        ],[
-        InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
-        ]]
+    buttons = [
+        [
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+        ]
+        ]
     return InlineKeyboardMarkup(buttons)
+
+
 
 
