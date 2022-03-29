ae0da429e4e3a91e2af426ba09c9017f66610225
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-29 18:10:08 +0530

    update AtwFilt.py

--- plugins/autofilter/AtwFilt.py
+++ plugins/autofilter/AtwFilt.py
@@ -3,13 +3,17 @@ from pyrogram import Client as illuzX, filters as Worker, emoji
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
 from plugins.database._utils import get_size
 from plugins.database.autofilter_db import get_search_results, is_subscribed
-from config import CACHE_TIME, AUTH_USERS, FORCES_SUB, CUSTOM_FILE_CAPTION
+from config import CACHE_TIME, AUTH_USERS, FORCES_SUB, CUSTOM_FILE_CAPTION , ADMINS
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or FORCES_SUB else CACHE_TIME
 
-@illuzX.on_message(Worker.command('Srch') & Worker.user(AUTH_USERS) if AUTH_USERS else None)
-def get_reply_markup(query):
-    buttons = [[
+@illuzX.on_message(Worker.private & Worker.command(["search"]))
+async def start_message(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(reply.from_user.id)
+    if len(reply.command) != 2:
+        if message.from_user.id not in ADMINS: 
+            buttons = [[
               InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat='')
         ]]
     return InlineKeyboardMarkup(buttons)
