e2600016b3711ede361e4599d0bb86552086ce39
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-29 16:38:44 +0530

    update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -255,20 +255,17 @@ async def cb_handler(client: illuzX, query):
             if query.from_user.id not in ADMINS: 
                 buttons = [[
                  InlineKeyboardButton("🔍𝐒𝐄𝐀𝐑𝐂𝐇🔎" ,switch_inline_query_current_chat='')
-                 ],[
+                ], [
                  InlineKeyboardButton("🔰𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥🔰", url="https://t.me/MalayalamOTTUpdatesMvb"),
-                 ],[
+                ], [
                  InlineKeyboardButton("🔸𝐌𝐨𝐯𝐢𝐞𝐬 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥🔸", url="https://t.me/my_TG_Bot2")
                  ]]
             else:
                 buttons = [[
                  InlineKeyboardButton("🔍𝐒𝐄𝐀𝐑𝐂𝐇🔎", switch_inline_query_current_chat='')
-                 ],[
-                 InlineKeyboardButton("ℹ️ Help", callback_data="bot_owner"),
-                 InlineKeyboardButton("😎 About", callback_data="about") 
-                 ],[
+                ], [
                  InlineKeyboardButton("🔰𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥🔰", url="https://t.me/MalayalamOTTUpdatesMvb"),
-                 ],[
+                ], [
                  InlineKeyboardButton("🔸𝐌𝐨𝐯𝐢𝐞𝐬 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥🔸", url="https://t.me/my_TG_Bot2")
                  ]]               
             await query.message.edit(text=START_MSG.format(mention=query.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
