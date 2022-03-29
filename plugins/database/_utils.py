59203b57c6839b1206802ae9ab0ea143d82ce042
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-29 16:53:45 +0530

    update _utils.py

--- plugins/database/_utils.py
+++ plugins/database/_utils.py
@@ -15,3 +15,14 @@ def get_size(size):
 def split_list(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
+        
+class temp(object):
+    BANNED_USERS = []
+    BANNED_CHATS = []
+    ME = None
+    CURRENT=int(os.environ.get("SKIP", 2))
+    CANCEL = False
+    MELCOW = {}
+    U_NAME = None
+    B_NAME = None
+    SETTINGS = {}
