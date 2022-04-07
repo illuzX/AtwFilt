488cc02963c012f219f461f764d773380cd4b236
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-07 22:26:18 +0530

    update totals.py

--- plugins/autofilter/totals.py
+++ plugins/autofilter/totals.py
@@ -5,22 +5,12 @@ from config import ADMINS
 logger = logging.getLogger(__name__)
 
 @illuzX.on_message(Worker.command('myDb') & Worker.user(ADMINS))
-async def status_handler(_, m: Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
-    await m.reply_text(
-        text=f"**Total Disk Space:** {total} \n"
-             f"**Used Space:** {used}({disk_usage}%) \n"
-             f"**Free Space:** {free} \n"
-             f"**CPU Usage:** {cpu_usage}% \n"
-             f"**RAM Usage:** {ram_usage}%\n\n"
-             f"**Total Users in DB:** `{total_users}`",
-        parse_mode="Markdown",
-        quote=True
-    )
+async def total(bot, message):
+
+    msg = await message.reply("Processing...⏳", quote=True)
+    try:
+        total = await Media.count_documents()
+        await msg.edit(f'📁 Saved files: {total}')
+    except Exception as e:
+        logger.exception('Failed to check total files')
+        await msg.edit(f'Error: {e}')
