2a7cd6de564c8ea3a43158820d77157c05f7f648
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-16 14:20:46 +0530

    update config.py

--- config.py
+++ config.py
@@ -43,10 +43,7 @@ DATABASE_NAME = environ.get("DATABASE_NAME", 'UserStorage2')
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 # ==================================
 
-team_name = os.environ.get('team_name','TeamCode')
-team_link = os.environ.get('team_link', 't.me/grayhathacker767'):
 
-# ==================================
 # ==================================
 # About Bot 🤖
 class bot_info(object):
