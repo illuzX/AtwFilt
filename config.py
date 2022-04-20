0ee4ff5d0322f44aaebd89c2545cdb6b93f5d5c5
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-21 05:01:51 +0530

    update config.py

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ def is_enabled(value, default):
         return default
 
 # ==================================
+class Config(object):
 API_ID = int(environ["API_ID"])
 API_HASH = environ["API_HASH"]
 B_KEYS = environ["BOT_TOKEN"]
