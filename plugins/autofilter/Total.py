2cd325b757f13c5f59da8037bd91581d29f8ae68
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-22 22:52:07 +0530

    update Total.py

--- plugins/autofilter/Total.py
+++ plugins/autofilter/Total.py
@@ -1,26 +1,20 @@
 import logging
-import shutil
-import psutil
 from pyrogram import Client as illuzX, filters as Worker
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from utils import get_size
 from plugins.database.autofilter_db import Media
-from plugins.database.users_chats_db import db
+from plugins.database.user_chat_db import db
+
 from config import ADMINS
 logger = logging.getLogger(__name__)
 
+@illuzX.on_message(Worker.command('myDb') & Worker.user(ADMINS))
+async def total(bot, message):
 
-@illuzX.on_message(Worker.command("mydb") & Worker.user(ADMINS) & ~Worker.edited)
-async def status(bot,  Message):
+    msg = await message.reply("Processing...⏳", quote=True)
+    try:
         total = await Media.count_documents()
-        users = await db.total_users_count()
-        chats = await db.total_chat_count()
-        monsize = await db.get_db_size()
-        free = 536870912 - monsize
-        monsize = get_size(monsize)
-        free = get_size(free)
-        await Message.edit_text(
-            text=AtwFilt.STATUS_TXT.format(total, users, chats, monsize, free),
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
+        await msg.edit(f'📁 Saved files: {total}')
+        users = await db.total_user_count()
+        await msg.edit(f'🌸Total users: {}')
+    except Exception as e:
+        logger.exception('Failed to check total files')
+        await msg.edit(f'Error: {e}')
