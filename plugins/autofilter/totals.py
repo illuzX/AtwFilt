826ed5030d70e0acb74161e72fe0ec6c12f99b2e
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-22 11:52:18 +0530

    update totals.py

--- plugins/autofilter/totals.py
+++ plugins/autofilter/totals.py
@@ -4,7 +4,6 @@ import psutil
 from pyrogram import Client as illuzX, filters as Worker
 from plugins.database.autofilter_db import Media
 from config import ADMINS
-from plugins.database.Printer import db
 from plugins.Addmin.runner import humanbytes
 logger = logging.getLogger(__name__)
 
@@ -18,14 +17,12 @@ async def status(bot,  Message):
     cpu_usage = psutil.cpu_percent()
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
     await m.reply_text(
         text=f"**Total Disk Space:** {total} \n"
              f"**Used Space:** {used}({disk_usage}%) \n"
              f"**Free Space:** {free} \n"
              f"**CPU Usage:** {cpu_usage}% \n"
-             f"**RAM Usage:** {ram_usage}%\n\n"
-             f"**Total Users in DB:** `{total_users}`",
+             f"**RAM Usage:** {ram_usage}%\n\n",
         parse_mode="Markdown",
         quote=True
     )
