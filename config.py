37e38340f1f497c43c0a35dab60db3f754bb8a28
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-22 11:49:47 +0530

    update config.py

--- config.py
+++ config.py
@@ -11,24 +11,23 @@ def is_enabled(value, default):
         return default
 
 # ==================================
-class fil_info (object):
-  API_ID = int(environ["API_ID"])
-  API_HASH = environ["API_HASH"]
-  B_KEYS = environ["BOT_TOKEN"]
-  START_MSG = environ.get("START_MSG",AtwFilt.DEFAULT_MSG)
-  BOT_PICS = (environ.get('PICS', 'https://telegra.ph/file/d5074341e29977da5ae60.jpg https://telegra.ph/file/5687297fe4b13efa8d595.jpg')).split()
-  SUPPORT = environ.get("SUPPORT", "https://www.google.com")
-  SPELL_MODE = is_enabled((environ.get('SPELL_MODE', "on")), True)
-  SET_SPEL_M = environ.get("SPELL_MODE_TEXT",AtwFilt.SPELL_CHECK)
-  LOG_CHANNEL = int(environ.get("LOG_CHANNEL", -1001645658297))
-  DATABASE_URI = environ.get("DATABASE_URI", None)
+API_ID = int(environ["API_ID"])
+API_HASH = environ["API_HASH"]
+B_KEYS = environ["BOT_TOKEN"]
+START_MSG = environ.get("START_MSG",AtwFilt.DEFAULT_MSG)
+BOT_PICS = (environ.get('PICS', 'https://telegra.ph/file/d5074341e29977da5ae60.jpg https://telegra.ph/file/5687297fe4b13efa8d595.jpg')).split()
+SUPPORT = environ.get("SUPPORT", "https://www.google.com")
+SPELL_MODE = is_enabled((environ.get('SPELL_MODE', "on")), True)
+SET_SPEL_M = environ.get("SPELL_MODE_TEXT",AtwFilt.SPELL_CHECK)
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", -1001645658297))
+DATABASE_URI = environ.get("DATABASE_URI", None)
 FORCE = environ.get('FORCES_SUB')
-  CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", AtwFilt.FILE_CAPTIONS)
-  DEV_NAME = environ.get("DEV_NAME", "Illuzx")
-  ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-  CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-  AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-  auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', "").split()]
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", AtwFilt.FILE_CAPTIONS)
+DEV_NAME = environ.get("DEV_NAME", "Illuzx")
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', "").split()]
 
 
 # ==================================
