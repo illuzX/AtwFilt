b31df4a676695bbafda7194401d36328a9bbcb1b
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-22 21:25:20 +0530

    update totals.py

--- plugins/autofilter/totals.py
+++ plugins/autofilter/totals.py
@@ -3,6 +3,7 @@ import shutil
 import psutil
 from pyrogram import Client as illuzX, filters as Worker
 from plugins.database.autofilter_db import Media
+from database.users_chats_db import db
 from config import ADMINS
 from plugins.Addmin.runner import humanbytes
 logger = logging.getLogger(__name__)
@@ -10,19 +11,15 @@ logger = logging.getLogger(__name__)
 
 @illuzX.on_message(Worker.command("status") & Worker.user(ADMINS) & ~Worker.edited)
 async def status(bot,  Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    await reply_text(
-        text=f"**Total Disk Space:** {total} \n"
-             f"**Used Space:** {used}({disk_usage}%) \n"
-             f"**Free Space:** {free} \n"
-             f"**CPU Usage:** {cpu_usage}% \n"
-             f"**RAM Usage:** {ram_usage}%\n\n",
-        parse_mode="Markdown",
-        quote=True
-    )
+        total = await Media.count_documents()
+        users = await db.total_users_count()
+        chats = await db.total_chat_count()
+        monsize = await db.get_db_size()
+        free = 536870912 - monsize
+        monsize = get_size(monsize)
+        free = get_size(free)
+        await query.message.edit_text(
+            text=script.STATUS_TXT.format(total, users, chats, monsize, free),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
