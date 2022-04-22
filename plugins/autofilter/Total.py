1394d60c2504609345d2e25147abe7798a83494d
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-22 22:53:35 +0530

    update Total.py

--- plugins/autofilter/Total.py
+++ plugins/autofilter/Total.py
@@ -9,12 +9,10 @@ logger = logging.getLogger(__name__)
 @illuzX.on_message(Worker.command('myDb') & Worker.user(ADMINS))
 async def total(bot, message):
 
-    msg = await message.reply("Processing...⏳", quote=True)
+    msg = await message.reply("LoOdIng...⏳🌸", quote=True)
     try:
         total = await Media.count_documents()
         await msg.edit(f'📁 Saved files: {total}')
-        users = await db.total_user_count()
-        await msg.edit(f'🌸Total users: {}')
     except Exception as e:
         logger.exception('Failed to check total files')
         await msg.edit(f'Error: {e}')
