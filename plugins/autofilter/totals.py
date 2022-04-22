a0cdf889362659f25f505e923eaa202f1b1c0cbc
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-22 21:30:28 +0530

    update totals.py

--- plugins/autofilter/totals.py
+++ plugins/autofilter/totals.py
@@ -3,13 +3,13 @@ import shutil
 import psutil
 from pyrogram import Client as illuzX, filters as Worker
 from plugins.database.autofilter_db import Media
+from plugins.database import runner
 from database.users_chats_db import db
 from config import ADMINS
-from plugins.Addmin.runner import humanbytes
 logger = logging.getLogger(__name__)
 
 
-@illuzX.on_message(Worker.command("status") & Worker.user(ADMINS) & ~Worker.edited)
+@illuzX.on_message(Worker.command("mydb") & Worker.user(ADMINS) & ~Worker.edited)
 async def status(bot,  Message):
         total = await Media.count_documents()
         users = await db.total_users_count()
@@ -19,7 +19,7 @@ async def status(bot,  Message):
         monsize = get_size(monsize)
         free = get_size(free)
         await query.message.edit_text(
-            text=script.STATUS_TXT.format(total, users, chats, monsize, free),
+            text=runner.STATUS_TXT.format(total, users, chats, monsize, free),
             reply_markup=reply_markup,
             parse_mode='html'
         )
