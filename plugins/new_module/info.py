3c32512b82d37db484d2132d9f39879b9a83f7a4
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-25 21:24:27 +0530

    update info.py

--- plugins/new_module/info.py
+++ plugins/new_module/info.py
@@ -13,7 +13,7 @@ from plugins.database.users_chats_db import db
 from plugins.new_module.run_cb import humanbytes
 @illuzx.on_message(filters.command("status") & filters.user(config.ADMINS) & ~filters.edited)
 async def status_handler(_, m: Message):
-    total, used, free= shutil.disk_usage(". ")
+    total, used, free = shutil.disk_usage(".")
     total = humanbytes(total)
     used = humanbytes(used)
     free = humanbytes(free)
