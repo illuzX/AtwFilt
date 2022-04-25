2e9750664e3a0ce9e49eb2eafd9afda169c439bd
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-25 20:57:53 +0530

    update info.py

--- plugins/new_module/info.py
+++ plugins/new_module/info.py
@@ -1,1 +1,31 @@
+# © illuzx
 
+import shutil
+import psutil
+#import client 
+from pyrogram import Client as illuzx, filters 
+from pyrogram.types import (
+    Message
+)
+
+
+@illuzx.on_message(filters.command("status") & filters.user(config.ADMINS) & ~filters.edited)
+async def status_handler(_, m: Message):
+    total, used, free= shutil.disk_usage(". ")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory ().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count ()
+    await m.reply_text(
+        text=f"**Total Disk Space:** {total} \n"
+             f"**Used Space:** {used}({disk_usage}%) \n"
+             f"**Free Space:** {free} \n"
+             f"**Cpu Usage:** {cpu_usage}% \n"
+             f"**Ram Usage:** {ram_usage}%\n\n"
+             f"**Total Users In db:** {total_users}`",
+        parse_mode="Markdown",
+        quote=True
+    )
