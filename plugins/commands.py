4668830d7b7d31c5b59eeb0683fb4599638232b9
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-26 05:47:50 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,41 +10,4 @@ db = Database()
 
 @illuzX.on_message(Worker.private & Worker.command(["start"]))
 async def start_message(bot, message):
-        await message.reply_photo(photo = choice(BOT_PICS), caption=START_MSG.format(mention = message.from_user.mention, bot_name = bot_info.BOT_NAME, bot_username = bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons))
-        
-    elif len(message.command) ==2 and message.command[1] in ["subscribe"]:
-        FORCES=["https://telegra.ph/file/10e57b92fde7aa7b540cf.jpg"]
-        invite_link = await bot.create_chat_invite_link(int(FORCES_SUB))
-        button=[[
-         InlineKeyboardButton("🦋Join To Use Me", url=invite_link.invite_link)
-         ]]
-        reply_markup = InlineKeyboardMarkup(button)
-        await message.reply_photo(
-            photo=choice(FORCES),
-            caption=f"""<i><b>Hello {message.from_user.mention}. \nYou Have <a href="{invite_link.invite_link}">Not Join</a> To <a href="{invite_link.invite_link}">My Update Channel</a>.So you do not get access To Files on Inline Mode, Bot Pm and Group</i></b>""",
-            reply_markup=reply_markup
-        )
-        return
-   
-@illuzX.on_message(Worker.private & Worker.command(["help"]))
-async def help(bot, message):
-    button = [[
-     InlineKeyboardButton("source code", callback_data="SOURCE_CODE"),
-     InlineKeyboardButton("About 😎", callback_data="about")
-     ]]
-    await message.reply_photo(
-        photo = choice(BOT_PICS),
-        caption=AtwFilt.HELP_MSG.format(mention=message.from_user.mention),
-        reply_markup=InlineKeyboardMarkup(button))
-      
-@illuzX.on_message(Worker.private & Worker.command(["about"]))
-async def about(bot, message):
-    button = [[
-     InlineKeyboardButton("🏠 Home", callback_data="start"),
-     InlineKeyboardButton("Close 🗑️", callback_data="close")
-     ]]  
-    await message.reply_photo(
-        photo = choice(BOT_PICS),
-        caption=AtwFilt.ABOUT_MSG.format(mention=message.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME, dev_name=DEV_NAME),
-        reply_markup=InlineKeyboardMarkup(button))
-        
+        await message.reply_photo(photo = choice(BOT_PICS), caption=START_MSG.format(mention = message.from_user.mention, bot_name = bot_info.BOT_NAME, bot_username = bot_info.BOT_USERNAME)
