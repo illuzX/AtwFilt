0aed55b112157253b4a8ecbe9e8e8a77d993f47d
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-26 05:40:02 +0530

    update startup.py

--- startup.py
+++ startup.py
@@ -1,16 +1,7 @@
 class AtwFilt(object):
 
     DEFAULT_MSG = """hy {mention}..
-    
-𝙷𝚎𝚕𝚕𝚘 [{bot_name}](t.me/{bot_username}) 𝙾𝚛 𝚢𝚘𝚞 𝚌𝚊𝚗 𝚌𝚊𝚕𝚕 𝚖𝚎 𝚊𝚜 [𝐅𝐢𝐥𝐭𝐞𝐫 𝐛𝐨𝐭 𝐯𝟐](t.me/{bot_username}) 
-
-𝙷𝚎𝚛𝚎 𝚈𝚘𝚞 𝙲𝚊𝚗 𝚁𝚎𝚚𝚞𝚎𝚜𝚝 𝙼𝚘𝚟𝚒𝚎'𝚜, 𝙹𝚞𝚜𝚝 𝚂𝚎𝚗𝚝 [𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎](t.me/{bot_username})
-𝚆𝚒𝚝𝚑 𝙿𝚛𝚘𝚙𝚎𝚛 #𝙶𝚘𝚘𝚐𝚕𝚎 𝚂𝚙𝚎𝚕𝚕𝚒𝚗𝚐..!!
-
-𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐃𝐞𝐭𝐚𝐢𝐥𝐬 𝐂𝐥𝐢𝐜𝐤 /help"""
-
-    DEFAULT_MSG_2
-    """Sorry 😐 this bot not work now 
+        Sorry 😐 this bot not work now 
     please try again later
 because im so sleepy 😪😴"""
 
