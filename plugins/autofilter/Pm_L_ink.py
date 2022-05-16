2a5a062b46f96499da12089ca8e7a1830e3d9951
illuzX <98640218+illuzX@users.noreply.github.com>
2022-05-16 13:15:48 +0530

    update Pm_L_ink.py

--- plugins/autofilter/Pm_L_ink.py
+++ plugins/autofilter/Pm_L_ink.py
@@ -4,6 +4,7 @@ import re
 from pyrogram import Client as illuzX, filters
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
 from config import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE, bot_info
+from config import bot_info
 from plugins.new_module.kanged import unpack_new_file_id
 from plugins.database.meow_pm import temp
 import re
@@ -36,7 +37,7 @@ async def gen_link_s(bot, message):
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"Here is your Link:\nhttps://t.me/{temp.B_NAME}?start={outstr}")
+    await message.reply(f"Here is your Link:\nhttps://t.me/{bot_info.BOT_USERNAME}?start={outstr}")
     
     
 @illuzX.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
@@ -79,7 +80,7 @@ async def gen_link_batch(bot, message):
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}_{cmd.lower().strip()}"
         b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-        return await sts.edit(f"Here is your link https://t.me/{temp.U_NAME}?start=DSTORE-{b_64}")
+        return await sts.edit(f"Here is your link https://t.me/{bot_info.BOT_USERNAME}?start=DSTORE-{b_64}")
 
     FRMT = "Generating Link...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
 
@@ -124,4 +125,4 @@ async def gen_link_batch(bot, message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
+    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{bot_info.BOT_USERNAME}?start=BATCH-{file_id}")
