c4d501a9557f9c6c988b5e1af017ccdb7fa985bc
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-06-01 21:39:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/illuzX/AtwFilt /AtwFilt
+  git clone https://github.com/Probotmaker8/pmautofilterbot /pmautofilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
