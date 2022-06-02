ab4715c743f066d8a2cc41d68421808abba64b0f
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-06-02 06:36:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,6 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
-from utils import temp
 
 class Bot(Client):
 
