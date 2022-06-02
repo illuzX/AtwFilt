445b55804972882cb8876042bdb24bc15ff98243
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-06-02 06:35:41 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,9 +9,23 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from utils import temp
 
 class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            session_name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
 
     async def start(self):
         b_users, b_chats = await db.get_banned()
