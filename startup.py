43aa53852695acdfb247f39e69e33e0b29f5f21b
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-02 21:12:19 +0530

    update startup.py

--- startup.py
+++ startup.py
@@ -2,11 +2,9 @@ class AtwFilt(object):
 
     START_TXT = """hy {mention}..
     
-Iam [{bot_name}](t.me/{bot_username}) 𝙾𝚛 𝚢𝚘𝚞 𝚌𝚊𝚗 𝚌𝚊𝚕𝚕 𝚖𝚎 𝚊𝚜 [𝐅𝐢𝐥𝐭𝐞𝐫 𝐛𝐨𝐭 𝐯𝟐](t.me/{bot_username}) 
-
+Iam [{bot_name}](t.me/{bot_username}) 𝙾𝚛 𝚢𝚘𝚞 𝚌𝚊𝚗 𝚌𝚊𝚕𝚕 𝚖𝚎 𝚊𝚜 [𝐅𝐢𝐥𝐭𝐞𝐫 𝐛𝐨𝐭 𝐯𝟐](t.me/{bot_username}) \n
 𝙷𝚎𝚛𝚎 𝚈𝚘𝚞 𝙲𝚊𝚗 𝚁𝚎𝚚𝚞𝚎𝚜𝚝 𝙼𝚘𝚟𝚒𝚎'𝚜, 𝙹𝚞𝚜𝚝 𝚂𝚎𝚗𝚝 [𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎](t.me/{bot_username})
-𝚆𝚒𝚝𝚑 𝙿𝚛𝚘𝚙𝚎𝚛 #𝙶𝚘𝚘𝚐𝚕𝚎 𝚂𝚙𝚎𝚕𝚕𝚒𝚗𝚐..!!
-
+𝚆𝚒𝚝𝚑 𝙿𝚛𝚘𝚙𝚎𝚛 #𝙶𝚘𝚘𝚐𝚕𝚎 𝚂𝚙𝚎𝚕𝚕𝚒𝚗𝚐..!!\n
 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐃𝐞𝐭𝐚𝐢𝐥𝐬 𝐂𝐥𝐢𝐜𝐤 /help"""
 
     HELP_MSG = """**
@@ -27,7 +25,7 @@ Iam [{bot_name}](t.me/{bot_username}) 𝙾𝚛 𝚢𝚘𝚞 𝚌𝚊𝚗 𝚌�
     ABOUT_MSG = """
 𝐀𝐁𝐎𝐔𝐓 𝐌𝐄
 
-○ 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : [𝐈𝐥𝐥𝐮𝐳𝕏](t.me/grayhathacker767)
+○ 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : [𝐈𝐥𝐥𝐮𝐳𝕏](t.me/illuzX)
 ○ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : [𝐏𝐲𝐭𝐡𝐨𝐧 𝟑.𝟗.𝟏𝟎](https://www.python.org/)
 ○ 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : [𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐀𝐬𝐲𝐧𝐜𝐢𝐨 𝟏.𝟒.𝟏𝟐](https://docs.pyrogram.org/)
 ○ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : [𝐔𝐳𝕏_𝐁𝐨𝐭𝐬](https://t.me/UZX_BOTS)
@@ -49,31 +47,31 @@ Size : <b>{size}</b>
 
     
 #~~Not work anymore~~
-    illuzX = """
-**ADMINS COMMANDS**
+   # illuzX = """
+#**ADMINS COMMANDS**
 
- » /broadcast - Reply Any Media Or Message
- » /delete - Reply Files
- » /deleteall - Delete All Files
- » /total - How Many Files Saved
- » /logger - Get Bot Logs
- » /channel - Add Channel List"""
+ #» /broadcast - Reply Any Media Or Message
+# » /delete - Reply Files
+# » /deleteall - Delete All Files
+ #» /total - How Many Files Saved
+ #» /logger - Get Bot Logs
+ #» /channel - Add Channel List"""
 # Close ????
 
     ADD_YOUR_GROUP = """**
 𝐼 𝐶𝑎𝑛'𝑡 𝐹𝑖𝑛𝑑  <i>#{query}</i> 𝑖𝑛 𝑚𝑦 𝑑𝑎𝑡𝑎𝑏𝑎𝑠𝑒 𝑠𝑜 𝑝𝑙𝑒𝑎𝑠𝑒 𝑐ℎ𝑒𝑐𝑘 𝑦𝑜𝑢'𝑟𝑒 𝑒𝑛𝑡𝑒𝑟𝑒𝑑 𝑠𝑝𝑒𝑙𝑙𝑖𝑛𝑔 𝑖𝑛 #𝐺𝑜𝑜𝑔𝑙𝑒/ 𝑂𝑟 𝑇ℎ𝑎𝑡 𝑀𝑜𝑣𝑖𝑒 𝑁𝑜𝑡 𝑅𝑒𝑙𝑒𝑎𝑠𝑒𝑑 /**"""
  
     SPELL_CHECK = """
-Hello 👋〘 {mention} 〙,
+Hello 👋<spoiler>〘 {mention} 〙, </spoiler>
 Couldn't Find {query}?  Please Click Your Request Movie Name"""
     GET_MOVIE_1 = """
 ** 📁 Here is What I Found In My Database** **For Your Query : #{title}**"""
 
     GET_MOVIE_2 = """
-📽️ Requested Movie : {query}
+📽️ Requested Movie :<b> {query}</b>
 
-👤 Requested By : {mention}
+👤 Requested By :<b> {mention} </b>
 
-Uploder :[MOVIE BAZZER](t.me/mvbzzer)
+Uploder : <b>{chatb</b>
+"""
 
-© **{chat}**"""
