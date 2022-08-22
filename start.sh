b88ec1432c7149fa372ee6f19c7288f441ba5156
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-22 09:19:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/illuzX/AtwFilt /AtwFilt
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AtwFilt
+  git clone $UPSTREAM_REPO /app
 fi
-cd /AtwFilt
+cd /app
 pip3 install -U -r requirements.txt
 echo "AtwFilt bot runing...."
 python3 main.py
